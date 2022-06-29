1936f273ca19c604d44d1aea04e863cdc1513e68
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-06-29 17:35:35 +0530

    First Commit

---
+++ src/main/java/net/learning/SpringBoot/Caffeine/Cache/SpringBootCaffeineCacheApplication.java
@@ -0,0 +1,13 @@
+package net.learning.SpringBoot.Caffeine.Cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaffeineCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCaffeineCacheApplication.class, args);
+	}
+
+}
