ebd75b395fa2f876e62f60ccf6fdf155f532af81
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-06-30 12:18:28 +0530

    Update caching in springboot app

---
+++ src/main/java/net/learning/SpringBoot/Caffeine/Cache/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package net.learning.SpringBoot.Caffeine.Cache;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
